25ab6c87d6ceb6d6dfe12d0b75fe212a11a0c3c8
miroslav221b <85048995+miroslav221b@users.noreply.github.com>
2023-04-18 20:26:59 +0300

    url update

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -2,4 +2,4 @@ export const ROUTES = {
     SHOP:"/",
     OFFER:"/offer"
 }
-export const BASE_URL = "https://catalog-api.onrender.com"
+export const BASE_URL = "https://catalog-api-8dru.onrender.com"
